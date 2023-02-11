59d82fc36e8e7fe765f1a2858a081ea6805264c8
mahmoudmaher2042 <115109404+mahmoudmaher2042@users.noreply.github.com>
2023-02-11 16:33:45 +0200

    Add files via upload

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled1/tile.dart';
 
 class Myapp extends StatefulWidget {
   const Myapp({Key key}) : super(key: key);
@@ -64,7 +64,19 @@ class _MyappState extends State<Myapp> {
             children: [
               Icon(Icons.camera_alt,size: 100,)
               ,Mylist(),
-              Mylist1(),Icon(Icons.camera_alt)
+              Container(color: Colors.white.withOpacity(0),height: double.infinity,child: Scaffold(extendBody: true,
+                  body: Mylist1(),drawerDragStartBehavior: DragStartBehavior.start,
+                  appBar: AppBar(backgroundColor: Colors.white,leadingWidth: double.infinity,leading: Head(),flexibleSpace: Container(
+                    child:Column(
+                      children: [SizedBox(height: 41,),
+                        Container(height: 15,color: Colors.grey.withOpacity(0.5),child: Row(
+                          children: [
+                            Text("Recent update",style: TextStyle(fontSize: 12),),
+                          ],mainAxisAlignment: MainAxisAlignment.start,
+                        ),),
+                      ],
+                    ) ,
+                  ),))),Mylist2()
             ],
           ),
         ));
@@ -89,6 +101,95 @@ class MyTile extends StatelessWidget {
     );
   }
 }
+class MyTile2 extends StatelessWidget {
+  MyTile2(this.index);
+
+  int index;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              radius: 35,
+              backgroundImage: AssetImage("assets/img/icon.jpg"),
+            ),
+            title: Row(
+              children: [
+                Text('engineer'),SizedBox(width: 180,),Icon(Icons.video_call),
+              ],
+            ),
+            subtitle: Row(
+              children: [Icon(Icons.call_missed_outlined,color: Colors.red,),
+                Text(' now'),
+              ],
+            ),
+            onTap: () {},
+            enabled: true,
+          ),
+        ],crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+    );
+  }
+}
+class MyTile1 extends StatelessWidget {
+  MyTile1(this.index);
+
+  int index;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Container(
+              width:62,height: 62,decoration: BoxDecoration(
+              color: Colors.blue,
+               borderRadius: BorderRadius.circular(110)
+            ),
+              child: CircleAvatar(
+                radius: 40,
+
+                backgroundImage: AssetImage("assets/img/icon.jpg"),
+              ),
+            ),
+            title: Text('engineer'),
+            subtitle: Text(' now'),
+            onTap: () {},
+            enabled: true,
+          ),
+        ],crossAxisAlignment: CrossAxisAlignment.start,
+      ),
+    );
+  }
+}
+class Head extends StatelessWidget {
+  const Head({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white.withOpacity(0),
+      child: Column(
+        children: [
+          Row(
+            children: [Stack(alignment:Alignment.bottomRight,children: [CircleAvatar(radius: 21,
+            backgroundImage: AssetImage("assets/img/icon.jpg"),),CircleAvatar(
+            radius: 5,backgroundColor: Colors.green,child: Icon(Icons.add
+            ,size: 8,),
+          )],
+          ),Column(
+              children: [Text("My statue",style: TextStyle(fontSize: 18,color: Colors.black),),Text("Tap to add statue update",style: TextStyle(fontSize: 7,color: Colors.black),),],
+            ),
+
+            ],mainAxisAlignment: MainAxisAlignment.start,
+          ),
+
+        ],
+      ),height: double.infinity,
+    );
+  }
+}
 
 
 class Mylist extends StatelessWidget {
@@ -103,7 +204,7 @@ class Mylist extends StatelessWidget {
         shrinkWrap: true,
         separatorBuilder: (context, index) => Center(
             child: Container(width: 400, height: 0.5, color: Colors.black)),
-        itemCount: 100,
+        itemCount: 10,
         itemBuilder: (context, index) => MyTile(index),
       ),
     );
@@ -121,13 +222,32 @@ class Mylist1 extends StatelessWidget {
         shrinkWrap: true,
         separatorBuilder: (context, index) => Center(
             child: Container(width: 400, height: 0.5, color: Colors.black)),
-        itemCount: 100,
-        itemBuilder: (context, index) => app(),
+        itemCount: 8,
+        itemBuilder: (context, index) => MyTile1(index),
+
       ),
     );
   }
 }
+class Mylist2 extends StatelessWidget {
+  const Mylist2({Key key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: ListView.separated(
+        shrinkWrap: true,
+        separatorBuilder: (context, index) => Center(
+            child: Container(width: 400, height: 0.5, color: Colors.black)),
+        itemCount: 8,
+        itemBuilder: (context, index) => MyTile2(index),
+
+      ),
+    );
+  }
+}
 void main() {
   runApp(MaterialApp(
     home: Myapp(),
